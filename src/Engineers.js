66d59f931a67e6af891791d06b31576d53a8aa34
Ian Busko <ian.busko@gmail.com>
2018-03-17 19:17:10 -0400

    Create table to list engineers

--- src/Engineers.js
+++ src/Engineers.js
@@ -19,6 +19,7 @@ class Engineers extends Component {
 
   render() {
     const { users } = this.state;
+    console.log(users);
     return (
       <article className='mw5 mw7-ns center bg-white pa3 ph5-ns sans-serif'>
         <header>
@@ -26,7 +27,27 @@ class Engineers extends Component {
         </header>
         <p className='lh-copy'>Select an engineer below to see how busy he or she is. Please don't bother busy engineers.</p>
 
-        
+        <table className="f6 w-100 mw8 center" cellSpacing="0">
+          <thead>
+            <tr className="stripe-dark">
+              <th className="fw6 tl pa3 bg-white">Status</th>
+              <th className="fw6 tl pa3 bg-white">Name</th>
+              <th className="fw6 tl pa3 bg-white">Email</th>
+            </tr>
+          </thead>
+          <tbody className="lh-copy">
+          {users.map((user, index) => {
+            return(
+              <tr className="stripe-dark">
+                <td className="pa3">Hassan Johnson</td>
+                <td className="pa3">{user.name}</td>
+                <td className="pa3">{user.email}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+
       </article>
     );
   }
