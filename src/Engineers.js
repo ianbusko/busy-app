5c58d1c90345c30dc3c419c4bb261334260768ea
Ian Busko <ian.busko@gmail.com>
2018-03-17 19:01:14 -0400

    Get Engineer data for Engineers page

--- src/Engineers.js
+++ src/Engineers.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
+import { getEngineers } from './api/engineersApi';
 
 class Engineers extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      users : [],
+      loaded: false
+    }
+  }
+
+  componentDidMount(){
+    getEngineers().then(data =>{
+      this.setState({ users: data.entity, loaded: true });
+      console.log(data.entity);
+    });
+  }
+
   render() {
+    const { users } = this.state;
     return (
-      <article>
+      <article className='mw5 mw7-ns center bg-white pa3 ph5-ns sans-serif'>
         <header>
-          <h1>All Engineers</h1>
+          <h1 className='mt0'>All Engineers</h1>
         </header>
+        <p className='lh-copy'>Select an engineer below to see how busy he or she is. Please don't bother busy engineers.</p>
+
         
       </article>
     );
