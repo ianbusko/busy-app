3e7ac1be61bc73b33b93d3b94372f5e23d5bd5a2
Ian Busko <ian.busko@gmail.com>
2018-03-17 18:05:33 -0400

    Remove unused link

--- src/components/header.js
+++ src/components/header.js
@@ -12,8 +12,7 @@ export default () => (
       <div className='bg-black-80 pb5 pb6-m pb6-l'>
         <nav className='dt w-100 mw8 center'>
           <div className='dtc tr v-mid pa3'>
-            <Link className='f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3' to="/">Find an Engineer</Link>
-            <Link className='f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3' to="/contact">Contact an Engineer</Link>
+            <Link className='f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3' to="/">Find</Link>
           </div>
         </nav>
 
