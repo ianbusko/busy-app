4e43d3b570ece5167a729bd5868fa9cefe17e811
Ian Busko <ian.busko@gmail.com>
2018-03-17 18:22:42 -0400

    Replace contents of Main.js with Switch

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import Home from "./Home";
-import Stuff from "./Stuff";
-import Contact from "./Contact";
-import Header from "./components/header.js";
+  Switch
+} from 'react-router-dom';
+import Home from './Home';
+import Stuff from './Stuff';
+import Contact from './Contact';
+import Header from './components/header.js';
 
 class Main extends Component {
   render() {
     return (
       <main>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
-            <Route path="/contact" component={Contact}/>
-          </div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/stuff' component={Stuff}/>
+          <Route path='/contact' component={Contact}/>
+        </Switch>
       </main>
     );
   }
