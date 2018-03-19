30ad82b1643bf2bf7e03e59ab0181a316d1d297c
Ian Busko <ian.busko@gmail.com>
2018-03-18 20:21:02 -0400

    Add tests that will fail when the API is not reachable

---
+++ src/api/__test__/engineersApi.test.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+
+const engineersApi = require('../engineersApi');
+
+// Test GetEngineer
+describe('#getEngineers() using promises', () => {
+  it('should load a list of engineers', ()=>{
+    return engineersApi.getEngineers()
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data.entity.length).toBeGreaterThan(0)
+    })
+  })
+})
