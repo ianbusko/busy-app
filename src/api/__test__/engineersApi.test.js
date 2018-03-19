471f387f828df4ad5c785a4346a386314d1b4ed1
Ian Busko <ian.busko@gmail.com>
2018-03-18 21:46:35 -0400

    Add tests for getEngineer

--- src/api/__test__/engineersApi.test.js
+++ src/api/__test__/engineersApi.test.js
@@ -3,13 +3,24 @@
 const engineersApi = require('../engineersApi')
 jest.mock('../request')
 
-// Test GetEngineer
+// Test GetEngineers
 describe('#getEngineers() using promises', () => {
   it('should load a list of engineers', ()=>{
     return engineersApi.getEngineers()
     .then(data => {
       expect(data).toBeDefined()
       expect(data.entity.length).toBeGreaterThan(0)
+    })
+  })
+})
+
+// Test GetEngineer
+describe('#getEngineer() using promises', () => {
+  it('should load an engineer', ()=>{
+    return engineersApi.getEngineer(2597479)
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data.entity.name).toEqual("James");
     })
   })
 })
