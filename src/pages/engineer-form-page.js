b57da9bd4f9024c6fbe11f9736b627d21dff173b
Ian Busko <ian.busko@gmail.com>
2018-03-24 19:34:27 -0400

    Add form to Engineer Form Page

--- src/pages/engineer-form-page.js
+++ src/pages/engineer-form-page.js
@@ -3,10 +3,39 @@ import { Redirect } from 'react-router';
 // import { SubmissionError } from 'redux-form';
 import { connect } from 'react-redux';
 import { fetchEngineer } from '../actions/engineer-actions';
+import EngineerForm from '../components/engineer-form';
 // import ContactForm from '../components/contact-form';
 
 
-class EngineerFormPage extends Component {}
+class EngineerFormPage extends Component {
+
+  state = {
+    redirect: false
+  }
+
+  componentDidMount = () => {
+    const { _id } = this.props.match.params;
+    if(_id){
+      this.props.fetchEngineer(_id)
+    } else {
+      // this.props.newContact();
+    }
+  }
+
+
+  render() {
+
+    return (
+      <article className='mw5 mw7-ns center bg-white pa3 ph5-ns sans-serif'>
+        <header>
+          <h1 className='mt0'>Selected Engineer</h1>
+        </header>
+        <p className='lh-copy'>Please edit your engineer's information below. </p>
+        <EngineerForm />
+      </article>
+    );
+  }
+}
 
 
 
